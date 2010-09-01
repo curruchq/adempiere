1e8945199f4df1ead24ef8de83b7a202b91c04fe
Josh Hill <josh.hill@conversant.co.nz>
2010-09-01 21:49:42 +0000

    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@85 e8211127-86c7-41ec-a2e1-12bbd99d3f81

--- customization_342/extend/src/org/compiere/model/MBillingRecord.java
+++ customization_342/extend/src/org/compiere/model/MBillingRecord.java
@@ -1,6 +1,7 @@
 package org.compiere.model;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
 import java.util.Properties;
 
 import org.compiere.util.CLogger;
@@ -8,7 +9,7 @@ import org.compiere.util.Env;
 
 import com.conversant.model.RadiusAccount;
 
-public class MBillingRecord extends X_MOD_BILLING_RECORD
+public class MBillingRecord extends X_MOD_Billing_Record
 {
 	/** Logger						*/
 	private static CLogger log = CLogger.getCLogger(MBillingRecord.class);
@@ -19,23 +20,24 @@ public class MBillingRecord extends X_MOD_BILLING_RECORD
 		if (MOD_BILLING_RECORD_ID == 0)
 		{
 			// TODO: Load defaults which aren't already defined at table level
-			setLINEDESC(null);
-			setRECORDSTATUS(null); // was STATUS
-			setSTATUSDATE(null);
-			setSYSTEMID(null);
 		}
 	}
 	
-	public static MBillingRecord createNew(Properties ctx, RadiusAccount account)
+	public MBillingRecord(Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+	
+	public static MBillingRecord createNew(Properties ctx, RadiusAccount account, String trxName)
 	{
 		if (ctx == null || account == null)
 			return null;
 		
-		MBillingRecord billingRecord = new MBillingRecord(ctx, 0, null);	
+		MBillingRecord billingRecord = new MBillingRecord(ctx, 0, trxName);	
 		
 		if (account.getRadAcctId() != null)
 		{
-			billingRecord.setRADACCTID(account.getRadAcctId()); 
+			billingRecord.setRadAcctId(account.getRadAcctId()); 
 		}
 		else
 		{
@@ -46,92 +48,11 @@ public class MBillingRecord extends X_MOD_BILLING_RECORD
 		billingRecord.setAD_Org_ID(Env.getAD_Org_ID(ctx));
 		billingRecord.setAD_Client_ID(Env.getAD_Client_ID(ctx));
 		
-		billingRecord.setACCTAUTHENTIC(account.getAcctAuthentic());
-		billingRecord.setACCTSESSIONID(account.getAcctSessionId());
-		billingRecord.setACCTSTARTTIME(account.getAcctStartTime());
-		billingRecord.setACCTSTOPTIME(account.getAcctStopTime());
-		billingRecord.setACCTTERMINATECAUSE(account.getAcctTerminateCause());
-		billingRecord.setACCTUNIQUEID(account.getAcctUniqueId());
-		billingRecord.setBILLINGID(account.getBillingId());
-		billingRecord.setCALLEDSTATIONID(account.getCalledStationId());
-		billingRecord.setCALLINGSTATIONID(account.getCallingStationId());
-		billingRecord.setCANONICALURI(account.getCanonicalURI());
-		billingRecord.setCONNECTINFO_START(account.getConnectInfo_start());
-		billingRecord.setCONNECTINFO_STOP(account.getConnectInfo_stop());
-		billingRecord.setCONTACT(account.getContact());
-		billingRecord.setDELAYTIME(account.getDelayTime());
-		billingRecord.setDESTINATIONID(account.getDestinationId());
-		billingRecord.setFRAMEDIPADDRESS(account.getFramedIPAddress());
-		billingRecord.setFRAMEDPROTOCOL(account.getFramedProtocol());
-		billingRecord.setFROMHEADER(account.getFromHeader());
-		billingRecord.setMEDIAINFO(account.getMediaInfo());
-		billingRecord.setNASIPADDRESS(account.getNASIPAddress());
-		billingRecord.setNASPORTID(account.getNASPortId());
-		billingRecord.setNASPORTTYPE(account.getNASPortType());
-		billingRecord.setNORMALIZED(account.getNormalized());
-		billingRecord.setRate(account.getRate());
-		billingRecord.setREALM(account.getRealm());
-		billingRecord.setRTPSTATISTICS(account.getRTPStatistics());
-		billingRecord.setSERVICETYPE(account.getServiceType());
-		billingRecord.setSIPAPPLICATIONTYPE(account.getSipApplicationType());
-		billingRecord.setSIPCODECS(account.getSipCodecs());
-		billingRecord.setSIPFROMTAG(account.getSipFromTag());
-		billingRecord.setSIPMETHOD(account.getSipMethod());
-		billingRecord.setSIPRPID(account.getSipRPID());
-		billingRecord.setSIPRPIDHEADER(account.getSipRPIDHeader());
-		billingRecord.setSIPTOTAG(account.getSipToTag());
-		billingRecord.setSIPTRANSLATEDREQUESTURI(account.getSipTranslatedRequestURI());
-		billingRecord.setSIPUSERAGENTS(account.getSipUserAgents());
-		billingRecord.setSOURCEIP(account.getSourceIP());
-		billingRecord.setSOURCEPORT(account.getSourcePort());
-		billingRecord.setTIMESTAMP(account.getTimestamp().toString());
-		billingRecord.setUserAgent(account.getUserAgent());
+		billingRecord.setAcctStartTime(account.getAcctStartTime());
+		billingRecord.setNormalized(account.getNormalized());
 		billingRecord.setUserName(account.getUserName());
-		
-		try
-		{
-			billingRecord.setACCTINPUTOCTETS(Long.toString(account.getAcctInputOctets()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setACCTINPUTOCTETS(null);
-		}
-		
-		try
-		{
-			billingRecord.setACCTOUTPUTOCTETS(Long.toString(account.getAcctOutputOctets()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setACCTOUTPUTOCTETS(null);
-		}
-		
-		try
-		{
-			billingRecord.setACCTSESSIONTIME(new BigDecimal(account.getAcctSessionTime()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setACCTSESSIONTIME(null);
-		}
-		
-		try
-		{
-			billingRecord.setACCTSTARTDELAY(Integer.toString(account.getAcctStartDelay()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setACCTSTARTDELAY(null);
-		}
-		
-		try
-		{
-			billingRecord.setACCTSTOPDELAY(Integer.toString(account.getAcctStopDelay()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setACCTSTOPDELAY(null);
-		}
+		billingRecord.setSipApplicationType(account.getSipApplicationType());
+		billingRecord.setRate(account.getRate());
 		
 		try
 		{
@@ -140,15 +61,6 @@ public class MBillingRecord extends X_MOD_BILLING_RECORD
 		catch (Exception ex)
 		{
 			billingRecord.setPrice(null);
-		}
-		
-		try
-		{
-			billingRecord.setSIPRESPONSECODE(Integer.toString(account.getSipResponseCode()));
-		}
-		catch (Exception ex)
-		{
-			billingRecord.setSIPRESPONSECODE(null);
 		}
 		
 		return billingRecord;
