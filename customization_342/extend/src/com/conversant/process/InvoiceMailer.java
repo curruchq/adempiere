96138d97286b712c48b2f70c1ba3395146160904
Josh Hill <josh.hill@conversant.co.nz>
2011-03-04 03:35:47 +0000

    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@149 e8211127-86c7-41ec-a2e1-12bbd99d3f81

--- customization_342/extend/src/com/conversant/process/InvoiceMailer.java
+++ customization_342/extend/src/com/conversant/process/InvoiceMailer.java
@@ -13,10 +13,13 @@ import org.compiere.db.CConnection;
 import org.compiere.interfaces.Server;
 import org.compiere.model.MClient;
 import org.compiere.model.MInvoice;
+import org.compiere.model.MMailText;
+import org.compiere.model.MUserMail;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.CLogger;
 import org.compiere.util.EMail;
+import org.compiere.util.Env;
 import org.compiere.util.Ini;
 
 public class InvoiceMailer extends SvrProcess
@@ -24,8 +27,11 @@ public class InvoiceMailer extends SvrProcess
 	/** Logger										*/
 	private static CLogger log = CLogger.getCLogger(InvoiceMailer.class);
 	
-	private int C_Invoice_ID = 0;
-	private String emailAddress = null;
+	private int R_MailText_ID;
+	
+	private int C_Invoice_ID;
+	
+	private String emailAddress;
 	
 	/**
 	 *  Prepare - e.g., get Parameters.
@@ -39,6 +45,10 @@ public class InvoiceMailer extends SvrProcess
 			String name = para[i].getParameterName();
 			if (para[i].getParameter() == null)
 				;
+			else if (name.equals("R_MailText_ID"))
+			{
+				R_MailText_ID = ((BigDecimal)para[i].getParameter()).intValue();
+			}
 			else if (name.equals("C_Invoice_ID"))
 			{
 				C_Invoice_ID = ((BigDecimal)para[i].getParameter()).intValue();
@@ -62,61 +72,69 @@ public class InvoiceMailer extends SvrProcess
 	@Override
 	protected String doIt() throws Exception
 	{
-		int MIN_SIZE = 2000; 	//	if not created size is 1015
+		return mailInvoice();
+	}
+	
+	private String mailInvoice()
+	{
+		// Load Message
+		MMailText mailText = new MMailText(getCtx(), R_MailText_ID, get_TrxName());
 		
+		// Load invoice
 		MInvoice invoice = MInvoice.get(getCtx(), C_Invoice_ID);
 		if (invoice == null)
 			return "@Error@ Invoice[" + C_Invoice_ID + "] does not exist";
 		
+		// Validate Email Address
 		if (!isEmailValid(emailAddress))
 			return "@Error@ Invalid email address '" + emailAddress + "'";
-
-		//		Check Directory
-		String dirName = getCtx().getProperty("documentDir", ".");
+		
+		// Check Directory
+		String directory = getCtx().getProperty("documentDir", ".");
 		try
 		{
-			File dir = new File (dirName);
-			if (!dir.exists ())
-				dir.mkdir ();
+			File dir = new File(directory);
+			if (!dir.exists())
+				dir.mkdir();
 		}
 		catch (Exception ex)
 		{
-			log.log(Level.SEVERE, "Could not create directory " + dirName, ex);
-			return "@Error@ Streaming error - directory";
+			log.log(Level.SEVERE, "Could not create directory " + directory, ex);
+			return "@Error@ Could not create directory for Invoices";
 		}
-		
-		//	Check if Invoice already created
-		String fileName = invoice.getPDFFileName(dirName);
+
+		// Check if invoice already created
+		String fileName = invoice.getPDFFileName(directory);
 		File file = new File(fileName);
-		if (file.exists() && file.isFile() && file.length() > MIN_SIZE)	
+		if (file.exists() && file.isFile() && file.length() > 2000)
 			log.info("Existing: " + file + " - " + new Timestamp(file.lastModified()));
 		else
 		{
 			log.info("New: " + fileName);
-			file = invoice.createPDF (file);
+			file = invoice.createPDF(file);
 			if (file != null)
 			{
-				invoice.setDatePrinted (new Timestamp(System.currentTimeMillis()));
+				invoice.setDatePrinted(new Timestamp(System.currentTimeMillis()));
 				invoice.save();
 			}
 		}
 		
-		EMail email = createEmail(emailAddress, "Invoice " + invoice.getDocumentNo(), "See attached invoice", true);
+		// Create email and add attachment
+		EMail email = createEmail(emailAddress, mailText.getMailHeader(), mailText.getMailText(true), mailText.isHtml());
 		email.addAttachment(file);
 		
+		// Send email and store response
 		String retValue = email.send();
+		
+		// Record email being sent
+		MUserMail um = new MUserMail(mailText, Env.getAD_User_ID(getCtx()), email);
+		um.save();
 		
 		return retValue;
 	}
 
 	public EMail createEmail(String to, String subject, String message, boolean html)
 	{
-		if (to == null || to.length() == 0)
-		{
-			log.warning("No To");
-			return null;
-		}
-		
 		EMail email = null;
 		MClient client = MClient.get(getCtx(), getAD_Client_ID());
 		if (client.isServerEMail() && Ini.isClient())
@@ -125,36 +143,24 @@ public class InvoiceMailer extends SvrProcess
 			try
 			{
 				if (server != null)
-				{ //	See ServerBean
-					email = server.createEMail(getCtx(), getAD_Client_ID(), to,
-							subject, message);
-				} else
+					email = server.createEMail(getCtx(), getAD_Client_ID(), to, subject, message);
+				else
 					log.log(Level.WARNING, "No AppsServer");
-			} catch (RemoteException ex)
+			}
+			catch (RemoteException ex)
 			{
 				log.log(Level.SEVERE, getName() + " - AppsServer error", ex);
 			}
 		}
-		String from = ""; // TODO
-		if (from == null || from.length() == 0)
-			from = client.getRequestEMail();
+		String from = client.getRequestEMail();
+
 		if (email == null)
 			email = new EMail(client, from, to, subject, message, html);
-		
-		//	Authorizetion
-		if (client.isSmtpAuthorization())
-		{
-//			if (getWStoreEMail() != null && getWStoreUser() != null && getWStoreUserPW() != null)
-//				email.createAuthenticator(getWStoreUser(), getWStoreUserPW());
-//			else
-				email.createAuthenticator(client.getRequestUser(), client.getRequestUserPW());
-		}
-		
-		//	Bcc
+
 		email.addBcc(from);
-		
+
 		return email;
-	} 
+	}
 	
 	/**
 	 *	Is Email address valid
