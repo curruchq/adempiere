35ff39c44b9fa028ac2a88db429a306af0dc16cd
Lavanya Nandyal <lavanya.nandyal@conversant.co.nz>
2012-06-04 11:59:03 +0000

    BNZ credit card payments
    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@356 e8211127-86c7-41ec-a2e1-12bbd99d3f81

---
+++ customization_342/base/src/org/compiere/model/I_C_BNZPaySchedule.java
@@ -0,0 +1,133 @@
+/**********************************************************************
+ * This file is part of Adempiere ERP Bazaar                          *
+ * http://www.adempiere.org                                           *
+ *                                                                    *
+ * Copyright (C) Trifon Trifonov.                                     *
+ * Copyright (C) Contributors                                         *
+ *                                                                    *
+ * This program is free software;
+ you can redistribute it and/or      *
+ * modify it under the terms of the GNU General Public License        *
+ * as published by the Free Software Foundation;
+ either version 2     *
+ * of the License, or (at your option) any later version.             *
+ *                                                                    *
+ * This program is distributed in the hope that it will be useful,    *
+ * but WITHOUT ANY WARRANTY;
+ without even the implied warranty of     *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the       *
+ * GNU General Public License for more details.                       *
+ *                                                                    *
+ * You should have received a copy of the GNU General Public License  *
+ * along with this program;
+ if not, write to the Free Software        *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         *
+ * MA 02110-1301, USA.                                                *
+ *                                                                    *
+ * Contributors:                                                      *
+ * - Trifon Trifonov (trifonnt@users.sourceforge.net)                 *
+ *                                                                    *
+ * Sponsors:                                                          *
+ * - Company (http://www.site.com)                                    *
+ **********************************************************************/
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for C_BNZPaySchedule
+ *  @author Trifon Trifonov (generated) 
+ *  @version Release 3.4.2s
+ */
+public interface I_C_BNZPaySchedule 
+{
+
+    /** TableName=C_BNZPaySchedule */
+    public static final String Table_Name = "C_BNZPaySchedule";
+
+    /** AD_Table_ID=1000013 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 4 - System 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(4);
+
+    /** Load Meta Data */
+
+    /** Column name Amount */
+    public static final String COLUMNNAME_Amount = "Amount";
+
+	/** Set Amount.
+	  * Amount in a defined currency
+	  */
+	public void setAmount (BigDecimal Amount);
+
+	/** Get Amount.
+	  * Amount in a defined currency
+	  */
+	public BigDecimal getAmount();
+
+    /** Column name C_BNZPaySchedule_ID */
+    public static final String COLUMNNAME_C_BNZPaySchedule_ID = "C_BNZPaySchedule_ID";
+
+	/** Set C_BNZPaySchedule	  */
+	public void setC_BNZPaySchedule_ID (int C_BNZPaySchedule_ID);
+
+	/** Get C_BNZPaySchedule	  */
+	public int getC_BNZPaySchedule_ID();
+
+    /** Column name C_BPartner_ID */
+    public static final String COLUMNNAME_C_BPartner_ID = "C_BPartner_ID";
+
+	/** Set Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public void setC_BPartner_ID (int C_BPartner_ID);
+
+	/** Get Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public int getC_BPartner_ID();
+
+	public I_C_BPartner getC_BPartner() throws Exception;
+
+    /** Column name C_Invoice_ID */
+    public static final String COLUMNNAME_C_Invoice_ID = "C_Invoice_ID";
+
+	/** Set Invoice.
+	  * Invoice Identifier
+	  */
+	public void setC_Invoice_ID (int C_Invoice_ID);
+
+	/** Get Invoice.
+	  * Invoice Identifier
+	  */
+	public int getC_Invoice_ID();
+
+	public I_C_Invoice getC_Invoice() throws Exception;
+
+    /** Column name IsPaid */
+    public static final String COLUMNNAME_IsPaid = "IsPaid";
+
+	/** Set Paid.
+	  * The document is paid
+	  */
+	public void setIsPaid (boolean IsPaid);
+
+	/** Get Paid.
+	  * The document is paid
+	  */
+	public boolean isPaid();
+
+    /** Column name PAYMENTDATE */
+    public static final String COLUMNNAME_PAYMENTDATE = "PAYMENTDATE";
+
+	/** Set PAYMENTDATE	  */
+	public void setPAYMENTDATE (Timestamp PAYMENTDATE);
+
+	/** Get PAYMENTDATE	  */
+	public Timestamp getPAYMENTDATE();
+}
