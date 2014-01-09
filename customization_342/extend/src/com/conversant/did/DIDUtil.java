be18831e7786a36cd336b45f0c0f92de62ec490e
Lavanya Nandyal <lavanya.nandyal@conversant.co.nz>
2014-01-09 10:38:07 +0000

    Added paid until date to the subscription
    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@482 e8211127-86c7-41ec-a2e1-12bbd99d3f81

--- customization_342/extend/src/com/conversant/did/DIDUtil.java
+++ customization_342/extend/src/com/conversant/did/DIDUtil.java
@@ -544,7 +544,7 @@ public class DIDUtil
 		return null;
 	}
 	
-	public static MSubscription createDIDSetupSubscription(Properties ctx, String number, int C_BPartner_ID, int C_BPartner_Location_ID, int M_Product_ID, Timestamp startDate, String trxName)
+	public static MSubscription createDIDSetupSubscription(Properties ctx, String number, int C_BPartner_ID, int C_BPartner_Location_ID, int M_Product_ID, Timestamp startDate,Timestamp paidUntilDate, String trxName)
 	{
 		HashMap<String, Object> fields = new HashMap<String, Object>();
 		
@@ -561,7 +561,7 @@ public class DIDUtil
 		fields.put(MSubscription.COLUMNNAME_M_Product_ID, M_Product_ID);
 		fields.put(MSubscription.COLUMNNAME_C_SubscriptionType_ID, DIDConstants.C_SUBSCRIPTIONTYPE_ID_ONE_OFF); 		
 		fields.put(MSubscription.COLUMNNAME_StartDate, startDate != null ? startDate : dates.get(MSubscription.COLUMNNAME_StartDate));
-		fields.put(MSubscription.COLUMNNAME_PaidUntilDate, dates.get(MSubscription.COLUMNNAME_PaidUntilDate)); 
+		fields.put(MSubscription.COLUMNNAME_PaidUntilDate, paidUntilDate != null ? paidUntilDate: dates.get(MSubscription.COLUMNNAME_PaidUntilDate)); 
 		fields.put(MSubscription.COLUMNNAME_RenewalDate, dates.get(MSubscription.COLUMNNAME_RenewalDate)); 
 		fields.put(MSubscription.COLUMNNAME_IsDue, false); 
 		fields.put(MSubscription.COLUMNNAME_BillInAdvance, false);
@@ -1633,4 +1633,29 @@ public class DIDUtil
 		
 		return subscribedNumbers;
 	}
+	//changes by lavanya 
+	/*public static MSubscription createDIDSetupSubscription(Properties ctx, String number, int C_BPartner_ID, int C_BPartner_Location_ID, int M_Product_ID, Timestamp startDate, Timestamp paidUntilDate ,String trxName)
+	{
+		HashMap<String, Object> fields = new HashMap<String, Object>();
+		
+		// Create name
+		String name = DIDConstants.DID_SUBSCRIPTION_NAME.replace(DIDConstants.NUMBER_IDENTIFIER, number);
+		
+		// Get dates
+		HashMap<String, Timestamp> dates = getSubscriptionDates(true, null);
+		
+		// Mandatory
+		fields.put(MSubscription.COLUMNNAME_Name, name);
+		fields.put(MSubscription.COLUMNNAME_C_BPartner_ID, C_BPartner_ID); 
+		fields.put(MBPartnerLocation.COLUMNNAME_C_BPartner_Location_ID, C_BPartner_Location_ID);
+		fields.put(MSubscription.COLUMNNAME_M_Product_ID, M_Product_ID);
+		fields.put(MSubscription.COLUMNNAME_C_SubscriptionType_ID, DIDConstants.C_SUBSCRIPTIONTYPE_ID_ONE_OFF); 		
+		fields.put(MSubscription.COLUMNNAME_StartDate, startDate != null ? startDate : dates.get(MSubscription.COLUMNNAME_StartDate));
+		fields.put(MSubscription.COLUMNNAME_PaidUntilDate,paidUntilDate!=null ? paidUntilDate: dates.get(MSubscription.COLUMNNAME_PaidUntilDate)); 
+		fields.put(MSubscription.COLUMNNAME_RenewalDate, dates.get(MSubscription.COLUMNNAME_RenewalDate)); 
+		fields.put(MSubscription.COLUMNNAME_IsDue, false); 
+		fields.put(MSubscription.COLUMNNAME_BillInAdvance, false);
+		
+		return createSubscription(ctx, fields, trxName);
+	}*/
 }
