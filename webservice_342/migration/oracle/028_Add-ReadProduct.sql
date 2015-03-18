c2efec2eea84c7ff8923f025c6f6631ab4c9e1f0
Lavanya Nandyal <lavanya.nandyal@conversant.co.nz>
2015-03-18 07:40:46 +0000

    Create readOrganization web service
    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@599 e8211127-86c7-41ec-a2e1-12bbd99d3f81

--- webservice_342/migration/oracle/028_Add-ReadProduct.sql
+++ webservice_342/migration/oracle/028_Add-ReadProduct.sql
@@ -2,7 +2,7 @@
 INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_DATE('2015-03-10 19:24:05','YYYY-MM-DD HH24:MI:SS'),100,'Y','readProduct',TO_DATE('2015-03-10 19:24:05','YYYY-MM-DD HH24:MI:SS'),100,'readProduct',50002,52039)
 ; 
 
--- ------------------------------------------------------ AD-readProductBPPrice-Intalio
+-- ------------------------------------------------------ AD-readProduct-Intalio
 INSERT INTO WS_WebServiceType (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID,WS_WebServiceType_ID) VALUES (0,0,TO_DATE('2015-03-10 19:46:34','YYYY-MM-DD HH24:MI:SS'),100,'','Y','AD-readProduct-Intalio',TO_DATE('2015-03-10 19:46:34','YYYY-MM-DD HH24:MI:SS'),100,'AD-readProduct-Intalio',50002,52039,50146)
 ;
 INSERT INTO WS_WebService_Para (AD_Client_ID,AD_Org_ID,ConstantValue,Created,CreatedBy,IsActive,ParameterName,ParameterType,Updated,UpdatedBy,WS_WebService_Para_ID,WS_WebServiceType_ID) VALUES (0,0,'1000000',TO_DATE('2015-03-10 19:47:23','YYYY-MM-DD HH24:MI:SS'),100,'Y','AD_Client_ID','C',TO_DATE('2015-03-10 19:47:23','YYYY-MM-DD HH24:MI:SS'),100,50438,50146)
