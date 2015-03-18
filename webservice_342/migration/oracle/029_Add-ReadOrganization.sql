4cd2f537a90cc9ad7edb2f17c2639e206c25d6cd
Lavanya Nandyal <lavanya.nandyal@conversant.co.nz>
2015-03-18 07:41:02 +0000

    Create readOrganization web service
    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@600 e8211127-86c7-41ec-a2e1-12bbd99d3f81

---
+++ webservice_342/migration/oracle/029_Add-ReadOrganization.sql
@@ -0,0 +1,16 @@
+-- ------------------------------------------------------ adminWebservice
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_DATE('2015-03-13 19:24:05','YYYY-MM-DD HH24:MI:SS'),100,'Y','readOrganization',TO_DATE('2015-03-13 19:24:05','YYYY-MM-DD HH24:MI:SS'),100,'readOrganization',50002,52040)
+; 
+
+-- ------------------------------------------------------ AD-readOrganization-Intalio
+INSERT INTO WS_WebServiceType (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID,WS_WebServiceType_ID) VALUES (0,0,TO_DATE('2015-03-13 19:46:34','YYYY-MM-DD HH24:MI:SS'),100,'','Y','AD-readOrganization-Intalio',TO_DATE('2015-03-13 19:46:34','YYYY-MM-DD HH24:MI:SS'),100,'AD-readOrganization-Intalio',50002,52040,50147)
+;
+INSERT INTO WS_WebService_Para (AD_Client_ID,AD_Org_ID,ConstantValue,Created,CreatedBy,IsActive,ParameterName,ParameterType,Updated,UpdatedBy,WS_WebService_Para_ID,WS_WebServiceType_ID) VALUES (0,0,'1000000',TO_DATE('2015-03-13 19:47:23','YYYY-MM-DD HH24:MI:SS'),100,'Y','AD_Client_ID','C',TO_DATE('2015-03-13 19:47:23','YYYY-MM-DD HH24:MI:SS'),100,50441,50147)
+;
+INSERT INTO WS_WebService_Para (AD_Client_ID,AD_Org_ID,ConstantValue,Created,CreatedBy,IsActive,ParameterName,ParameterType,Updated,UpdatedBy,WS_WebService_Para_ID,WS_WebServiceType_ID) VALUES (0,0,'1000001',TO_DATE('2015-03-13 19:47:43','YYYY-MM-DD HH24:MI:SS'),100,'Y','AD_Org_ID','C',TO_DATE('2015-03-13 19:47:43','YYYY-MM-DD HH24:MI:SS'),100,50442,50147)
+;
+INSERT INTO WS_WebService_Para (AD_Client_ID,AD_Org_ID,ConstantValue,Created,CreatedBy,IsActive,ParameterName,ParameterType,Updated,UpdatedBy,WS_WebService_Para_ID,WS_WebServiceType_ID) VALUES (0,0,'50004',TO_DATE('2015-03-13 19:47:51','YYYY-MM-DD HH24:MI:SS'),100,'Y','AD_Role_ID','C',TO_DATE('2015-03-13 19:47:51','YYYY-MM-DD HH24:MI:SS'),100,50443,50147)
+;
+INSERT INTO WS_WebServiceTypeAccess (AD_Client_ID,AD_Org_ID,AD_Role_ID,Created,CreatedBy,IsActive,IsReadWrite,Updated,UpdatedBy,WS_WebServiceType_ID) VALUES (0,0,50004,TO_DATE('2015-03-13 19:48:12','YYYY-MM-DD HH24:MI:SS'),100,'Y','Y',TO_DATE('2015-03-13 19:48:12','YYYY-MM-DD HH24:MI:SS'),100,50147)
+;
+
