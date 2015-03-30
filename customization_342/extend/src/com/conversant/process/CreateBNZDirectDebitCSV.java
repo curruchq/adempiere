3659a3115543d2c36f654989ea1c1bfeb792ed71
Lavanya Nandyal <lavanya.nandyal@conversant.co.nz>
2015-03-30 08:22:58 +0000

    Bug Fix:BNZ direct debit control total incorrect
    git-svn-id: http://svn.conversant.co.nz/adempiere/trunk@612 e8211127-86c7-41ec-a2e1-12bbd99d3f81

--- customization_342/extend/src/com/conversant/process/CreateBNZDirectDebitCSV.java
+++ customization_342/extend/src/com/conversant/process/CreateBNZDirectDebitCSV.java
@@ -283,12 +283,12 @@ public class CreateBNZDirectDebitCSV extends SvrProcess {
 			fileWriter.append(String.valueOf(count)); // Transaction record count
 			fileWriter.append(COMMA_DELIMITER);
 			String str=new String(hashtotal.toString());
-			/*if(str.length() < 11)
+			if(str.length() < 11)
 				str=String.format("%011d", Long.parseLong(str));
 			else if(str.length() > 11)
 				str = str.substring(1);
 			fileWriter.append(str); // Hash Total
-*/			fileWriter.append((str.length() < 11) ? String.format("%011d", Long.parseLong(str)) : str.substring(1));
+
 			
 			
 		}
